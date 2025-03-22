54873afe1f46007619e11ab45f68130f2ee163a5
A283le <alexdominguez169@gmail.com>
2025-03-22 15:01:26 -0400

    Fundamentals completed.

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,6 +1,23 @@
 describe('Fundamentals Test', () => {
-  it('Initial Test', () => {
-    cy.visit('http://localhost:3000')
-    cy.get('body > main > h1').contains('Why')
+  // there is alot of overhead between tests, best to do big ones.
+  beforeEach(() => {
+    cy.visit('/')
+    cy.getDataTest('fundamentals-header').contains('Why')
+
+    cy.getDataTest('fundamentals-header').should('contain.text', 'Why')
+  })
+
+  // can turn this into it.only to work on one test at a time
+  it('Accodrion Works as Intended', () => {
+    // assert closed by default
+    cy.contains("all-in-one").filter(":visible").should('not.exist')
+    
+    // assert when clicked the correct text shows
+    cy.get('[data-test="accordion-item-1"]').click()
+    cy.contains("all-in-one").filter(":visible").should('exist')
+    
+    // assert when clicked again it hides
+    cy.get('[data-test="accordion-item-1"]').click()
+    cy.contains("all-in-one").filter(":visible").should('not.exist')
   })
 })
